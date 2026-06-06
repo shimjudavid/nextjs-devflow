c2147d216fa68d1d4bd5257c963ebd2c65861242
Shimju David <shimjudavid@gmail.com>
2026-06-06 12:15:06 +0530

    Implement Eslint & Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import React from 'react';
-
 const Home = () => {
   return (
-    <div className="text-3xl text-violet-700 font-black">
-      Welcome to Next.js
-    </div>
+    <div className='flex items-center rounded bg-blue-500 p-4 text-white'>Welcome to Nesxt.js</div>
   );
 };
 
